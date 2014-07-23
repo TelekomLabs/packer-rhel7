1ba4e8806792fb817520b9eda4b55dd9f4d52997
Christoph Hartmann <chris@lollyrock.com>
2014-07-23 18:44:58 +0200

    update Grub configuration

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -1,25 +1,30 @@
 # Configure serial console
 yum -y install grub2-tools
 
+# https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/7/html/System_Administrators_Guide/sec-GRUB_2_over_Serial_Console.html#sec-Configuring_GRUB_2
 cat > /etc/default/grub <<EOF
-GRUB_TIMEOUT=5
-GRUB_DISTRIBUTOR="\$(sed 's, release .*\$,,g' /etc/system-release)"
+GRUB_DEFAULT=0
+GRUB_HIDDEN_TIMEOUT=0
+GRUB_HIDDEN_TIMEOUT_QUIET=true
+GRUB_TIMEOUT=1
+GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
 GRUB_DEFAULT=saved
 GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL=serial
+GRUB_SERIAL_COMMAND="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1"
+GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"
 GRUB_TERMINAL_OUTPUT="console"
-GRUB_CMDLINE_LINUX="rd.lvm.lv=centos/swap crashkernel=auto  rd.lvm.lv=centos/root vconsole.font=latarcyrheb-sun16 vconsole.keymap=de-latin1 rhgb console=ttyS0"
 GRUB_DISABLE_RECOVERY="true"
 EOF
 
 grub2-mkconfig -o /boot/grub2/grub.cfg
-
 
 # remove uuid
 sed -i '/UUID/d' /etc/sysconfig/network-scripts/ifcfg-e*
 sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-e*
 
 # Installs cloudinit, epel is required
-yum -y install cloud-init
+#yum -y install cloud-init
 
 # configure cloud init 'cloud-user' as sudo
 # this is not configured via default cloudinit config
@@ -38,6 +43,6 @@ EOL
 yum -y install haveged
 
 # replace password from root with a encrypted one
-# usermod -p "*" root
+#usermod -p "*" root
 
 
