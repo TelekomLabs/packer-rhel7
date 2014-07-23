221bc7da17c4ba767fccef6c383d9e56d3b904b5
Eric S. <Korni22@users.noreply.github.com>
2014-07-23 13:04:56 +0200

    Now proper cat, not using wget.

--- scripts/bootlogs.sh
+++ scripts/bootlogs.sh
@@ -1,3 +1,11 @@
-wget https://cdn.rawgit.com/Korni22/b2d05cbb1836efd79642/raw/e87d0e571aba1efbb9582f70af6d158ef60091e9/grub
-mv grub /etc/default/grub
+cat > /etc/default/grub <<EOF
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR="\$(sed 's, release .*\$,,g' /etc/system-release)"
+GRUB_DEFAULT=saved
+GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_CMDLINE_LINUX="rd.lvm.lv=centos/swap crashkernel=auto  rd.lvm.lv=centos/root vconsole.font=latarcyrheb-sun16 vconsole.keymap=de-latin1 rhgb console=ttyS0"
+GRUB_DISABLE_RECOVERY="true"
+EOF
+
 grub2-mkconfig -o /boot/grub2/grub.cfg
