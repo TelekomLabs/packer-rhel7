bc0f5784d3a971c5386840a9685a70d073e27ba2
Christoph Hartmann <chris@lollyrock.com>
2015-07-19 14:55:06 +0200

    use latest epel package

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -1,6 +1,6 @@
 # install official epel package
 # @see https://fedoraproject.org/wiki/EPEL
 rpm --import https://fedoraproject.org/static/0608B895.txt
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
 yum -y update
