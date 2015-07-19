7745e184730932ecfa0edb8b42a07519eb48f0db
Christoph Hartmann <chris@lollyrock.com>
2015-07-19 15:34:58 +0200

    improve image cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,16 @@
+# delete packages
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
+
+# clean yum history
+yum history new
+yum -y clean all
+
+# clean up logs
+truncate -c -s 0 /var/log/yum.log
+rm -rf /var/log/anaconda*
+rm -rf /var/lib/yum/*
+rm -rf /var/lib/random-seed
+rm -rf /root/install.log
+rm -rf /root/install.log.syslog
+rm -rf /root/anaconda-ks.cfg
